5ca256c7ba4bf734cc305e55d63169d1364f16df
reolino <reolino@gmail.com>
2019-07-02 17:33:45 +0900

    에러 컨트롤러 수정

--- jejuplan/src/main/java/com/jejuplan/common/CustomErrorController.java
+++ jejuplan/src/main/java/com/jejuplan/common/CustomErrorController.java
@@ -28,6 +28,7 @@ public class CustomErrorController implements ErrorController {
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+        log.info("httpStatus : "+httpStatus.toString());
 
         model.addAttribute("code", status.toString());
         model.addAttribute("msg", httpStatus.getReasonPhrase());
