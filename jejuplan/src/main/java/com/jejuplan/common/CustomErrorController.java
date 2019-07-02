bc06ea9b8d7f5e140c9d6581ac2325c1563f3120
reolino <reolino@gmail.com>
2019-07-02 17:24:32 +0900

    error 페이지

---
+++ jejuplan/src/main/java/com/jejuplan/common/CustomErrorController.java
@@ -0,0 +1,43 @@
+package com.jejuplan.common;
+
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+ 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+import lombok.extern.slf4j.Slf4j;
+ 
+@Controller
+@Slf4j
+public class CustomErrorController implements ErrorController {
+ 
+    private static final String ERROR_PATH = "/error";
+ 
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+    
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+        log.info("httpStatus : "+httpStatus.toString());
+        log.info("code : "+ status.toString() );
+        log.info("msg : "+ httpStatus.getReasonPhrase());
+        log.info("timestamp : "+new Date());
+
+        model.addAttribute("code", status.toString());
+        model.addAttribute("msg", httpStatus.getReasonPhrase());
+        model.addAttribute("timestamp", new Date());
+        
+        return "common/error";
+    }
+ 
+}
