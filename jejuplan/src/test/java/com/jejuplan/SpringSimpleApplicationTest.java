1fbff6c82c1ea835059bb11860490ea002c4f875
reolino <reolino@gmail.com>
2019-07-19 16:49:50 +0900

    war deploy

--- jejuplan/src/test/java/com/jejuplan/SpringSimpleApplicationTest.java
+++ jejuplan/src/test/java/com/jejuplan/SpringSimpleApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.jejuplan;
+package com.jejuplan;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringSimpleApplicationTests {
+public class SpringSimpleApplicationTest {
 
 	@Test
 	public void contextLoads() {
